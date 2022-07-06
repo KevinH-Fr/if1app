81d1f667586cbaf52faa0313ff4d94bb605b8885
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-07 00:03:03 +0200

    ajout saison

---
+++ app/models/saison.rb
@@ -0,0 +1,3 @@
+class Saison < ApplicationRecord
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :friends
+  resources :saisons
+
   get 'home/index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
