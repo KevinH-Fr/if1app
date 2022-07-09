93f7fe0e560e26f17cac0d1f2a28f6939f824b46
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-09 17:58:11 +0200

    ajout base devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+
+  devise_for :users
   resources :friends
   resources :saisons
   resources :divisions
@@ -11,7 +15,8 @@ Rails.application.routes.draw do
   resources :classements
 
   get 'home/index'
-  root 'home#index'
+ # root 'home#index'
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
