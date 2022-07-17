f3f756560043300b03705e24595be6a6ed13c8c2
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-17 14:47:56 +0200

    ajout scopes resultat et call dans cotes

--- app/controllers/cotes_controller.rb
+++ app/controllers/cotes_controller.rb
@@ -1,19 +1,12 @@
 class CotesController < ApplicationController
-    before_action :set_cote, only: %i[ show edit update destroy ]
+  before_action :set_cote, only: %i[ show edit update destroy ]
   
     def index
-      @classements = Classement.all
+     
       @divisions = Division.all
       @events = Event.all
       @saisons = Saison.all
-      @pilotes = Pilote.all
-      @resultats = Resultat.all
-      @resultatsFiltres = Resultat.all
-  
-      @resultat = Resultat.all
-  
-      @q = Classement.ransack(params[:q])
-  
+     
       if params[:saisonId]
         @saisonId = params[:saisonId]
       end
@@ -28,48 +21,11 @@ class CotesController < ApplicationController
       if params[:eventId]
         @eventId = params[:eventId]
         @eventNum = Event.find(@eventId).numero 
-  
-       # @divisionId = Event.find(@eventId).division_id 
-  
-      #  @resultats = Resultat.all
 
-      # @resultats_division = Resultat.division_courant(@divisionId).all
-     
-      # @resultats_saison = Resultat.saison_courant(@saisonId).all
+        @resultats_filtres = Resultat.saison_courant(@saisonId).division_courant(@divisionId).numero_until_courant(@eventNum)
+                                     .group_sum_order
 
-       #@resultats_until_numero_event = Resultat.numero_until_courant(@eventNum).all
-
-        @resultats_filtres = Resultat.saison_courant(@saisonId)
-                                          .division_courant(@divisionId)
-                                          .numero_until_courant(@eventNum).all
-
-
-
-
-      #  @resultats = Resultat.event_courant(@eventId).all
-
-      #  @pilotes = Pilote.all
-      #  @pilotesActifDiv = Pilote.all.where(statut: "actif", division_id: @divisionId)  
-  
-       #   @resultatsFiltres = Resultat.joins(:event).where(
-       #     'numero <= :numero AND 
-       #      saison_id = :saison_id AND 
-       #      division_id = :division_id',  
-       #      numero: params[:numGp],
-       #      saison_id: params[:saisonId],
-       #      division_id: params[:divisionId])
-  
-
-
-
-     # @pilotes = @pilotes.filter_by_division(params[:divisionId]) if params[:divisionId].present?
-   #   @pilotes = Pilote.statut_actif.division_courant(@divisionId).all
-            
       else
-        
-        @resultats = Resultat.all
-    #    @resultatsFiltres = Resultat.all
-    #    @pilotesActifDiv = Pilote.all
       end
   
       respond_to do |format|
@@ -80,17 +36,14 @@ class CotesController < ApplicationController
         end
       end
   
-  
     end
   
     private
   
-      # Use callbacks to share common setup or constraints between actions.
       def set_cote
         @cote = Cote.find(params[:id])
       end
   
-      # Only allow a list of trusted parameters through.
       def cote_params
         params.fetch(:cote, {})
       end
--- app/models/resultat.rb
+++ app/models/resultat.rb
@@ -1,6 +1,6 @@
 class Resultat < ApplicationRecord
 
-    enum positions: {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 
+  enum positions: {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 
                      6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10,
                      11 => 11, 12 => 12, 13 => 13, 14 => 14, 15 => 15,
                      16 => 16, 17 => 17, 18 => 18, 19 => 19, 20 => 20
@@ -13,14 +13,22 @@ class Resultat < ApplicationRecord
     validates :pilote_id, presence: true
     validates :ecurie, presence: true
 
-
     scope :with_mt, -> { where("(mt) = true") }
 
 scope :division_courant, -> (division_courant) { joins(:event).where("division_id = ?", division_courant)}
 scope :saison_courant, -> (saison_courant) { joins(:event).where("saison_id = ?", saison_courant)}
+scope :event_courant, -> (event_courant) { where("event_id <= ?", event_courant)}
 
 scope :numero_until_courant, -> (numero_until_courant) { joins(:event).where("numero <= ?", numero_until_courant)}
 
-scope :event_courant, -> (event_courant) { where("event_id <= ?", event_courant)}
+# tempo
+scope :group_by_pilote, -> { group('pilote_id') }
+scope :sum_by_pilote, -> { sum('score') }
+scope :order_by_sum, -> { order('score') }
+scope :sum_order_score, -> { select('pilote_id, score, SUM(score) AS total')}
+
+# utilisée
+scope :group_sum_order, -> { select('pilote_id, SUM(score) AS total').group('pilote_id').order('total').reverse }
+
 
 end
