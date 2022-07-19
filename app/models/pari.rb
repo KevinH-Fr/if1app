9a00603e309e914e48caa77f17c3c325566e3bfd
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-19 21:01:54 +0200

    pari calcul solde si resultat true

--- app/models/pari.rb
+++ app/models/pari.rb
@@ -3,6 +3,8 @@ class Pari < ApplicationRecord
     belongs_to :coureur, :class_name => 'Pilote', :optional => true
 
     validates :coureur_id, presence: true
+    validates :parieur_id, presence: true
+    
     validates :typepari, presence: true
    
 
@@ -13,5 +15,13 @@ class Pari < ApplicationRecord
 
     scope :event_courant, -> (event_courant) { where(event_id: event_courant)}
 
+    after_initialize :set_default_montant, :set_default_cote, :if => :new_record?
+    def set_default_montant
+      self.montant ||= 0
+    end
+
+    def set_default_cote
+        self.cote ||= 0
+      end
 
 end
