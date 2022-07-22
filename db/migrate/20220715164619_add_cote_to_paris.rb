5d7e27ac916b9c56647364471aaaf24095312535
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-22 19:23:41 +0200

    modifs

--- db/migrate/20220715164619_add_cote_to_paris.rb
+++ db/migrate/20220715164619_add_cote_to_paris.rb
@@ -1,5 +1,7 @@
 class AddCoteToParis < ActiveRecord::Migration[7.0]
   def change
-    add_column :paris, :cote, :decimal
+    unless column_exists? :paris, :cote
+      add_column :paris, :cote, :decimal
+    end
   end
 end
