bc9b0e75600f822900564989cd5fe08506d5fb7f
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-22 22:50:01 +0200

    modifs

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module If1app
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoloader = :classic
+    
   end
 end
