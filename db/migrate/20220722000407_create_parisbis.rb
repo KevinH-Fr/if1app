c87fe79832edd8cf622d541b5a6bde074f492756
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-22 20:42:30 +0200

    'modifs

--- db/migrate/20220722000407_create_parisbis.rb
+++
@@ -1,8 +0,0 @@
-class CreateParisbis < ActiveRecord::Migration[7.0]
-  def change
-    create_table :parisbis do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20220722105736_add_event_to_parisbis.rb
+++
@@ -1,5 +0,0 @@
-class AddEventToParisbis < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :parisbis, :event, null: false, foreign_key: true
-  end
-end
